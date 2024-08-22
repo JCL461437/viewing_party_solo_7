45dc0dd6fea73b60f493ed1b64b100a3dbc4cbef
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-22 09:51:07 -0600

    Add the not refactored index action outlight for a connection to the api with the desired endpoint for us 2 functionality

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,6 +2,15 @@ class DiscoverController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.headers["X-API-Key"] = Rails.application.credentials.movie_db[:key]
+    end
+
+    response = conn.get("/v3/member/#{@user.id}?limit=20")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @top_20_movies = json[:members]
   end
 
   private 
