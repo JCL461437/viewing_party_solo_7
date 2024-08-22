507191596dfefe850d5e7592d4e0a061b3a4fbd0
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-22 10:48:56 -0600

    Clean out the index action for the discover controller

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -2,15 +2,6 @@ class DiscoverController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.headers["X-API-Key"] = Rails.application.credentials.movie_db[:key]
-    end
-
-    response = conn.get("/3/discover/movie")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-    @top_20_movies = json[:results]
   end
 
   private 
