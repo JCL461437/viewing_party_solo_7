dbf96c104c3b023a6dae0f3e262adc27363c2ad6
Jan Lehigh <janlehigh461437@gmail.com>
2024-08-22 10:48:24 -0600

    Change the placement of the api connection and endpoint placement from discover index to the movies controller index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,14 @@
 class MoviesController < ApplicationController
   def index
-    
+    @user = User.find(params[:user_id])
+
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.headers["X-API-Key"] = Rails.application.credentials.movie_db[:key]
+    end
+
+    response = conn.get("/3/discover/movie")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @top_20_movies = json[:results]
   end
 end
